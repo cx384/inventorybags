ae91c03e3dd823a62d71552493352ca7ee0542f9
cx384 <muelladresse84@web.de>
2017-12-30 15:14:02 +0100

    fix sort_inventory function

--- sort_inventory_function.lua
+++ sort_inventory_function.lua
@@ -15,42 +15,42 @@ function inventorybags.sort_inventory(inv)
  			local m = st:get_metadata()
  			local k = string.format("%s %05d %s", n, w, m)
  			if not typecnt[k] then
--				typecnt[k] = {
--					name = n,
--					wear = w,
--					metadata = m,
--					stack_max = st:get_stack_max(),
--					count = 0,
--				}
-+				typecnt[k] = {st}
+				typecnt[k] = {
+					name = n,
+					wear = w,
+					metadata = m,
+					stack_max = st:get_stack_max(),
+					count = 0,
+				}
+				typecnt[k] = {st}
  				table.insert(typekeys, k)
-+			else
-+				table.insert(typecnt[k], st)
+			else
+				table.insert(typecnt[k], st)
  			end
--			typecnt[k].count = typecnt[k].count + st:get_count()
+			typecnt[k].count = typecnt[k].count + st:get_count()
  		end
  	end
  	table.sort(typekeys)
--	local outlist = {}
-+	inv:set_list("main", {})
+	local outlist = {}
+	inv:set_list("main", {})
  	for _, k in ipairs(typekeys) do
--		local tc = typecnt[k]
--		while tc.count > 0 do
--			local c = math.min(tc.count, tc.stack_max)
--			table.insert(outlist, ItemStack({
--				name = tc.name,
--				wear = tc.wear,
--				metadata = tc.metadata,
--				count = c,
--			}))
--			tc.count = tc.count - c
-+		for _, item in ipairs(typecnt[k]) do
-+			inv:add_item("main", item)
+		local tc = typecnt[k]
+		while tc.count > 0 do
+			local c = math.min(tc.count, tc.stack_max)
+			table.insert(outlist, ItemStack({
+				name = tc.name,
+				wear = tc.wear,
+				metadata = tc.metadata,
+				count = c,
+			}))
+			tc.count = tc.count - c
+		for _, item in ipairs(typecnt[k]) do
+			inv:add_item("main", item)
  		end
  	end
--	if #outlist > #inlist then return end
--	while #outlist < #inlist do
--		table.insert(outlist, ItemStack(nil))
--	end
--	inv:set_list("main", outlist)
+	if #outlist > #inlist then return end
+	while #outlist < #inlist do
+		table.insert(outlist, ItemStack(nil))
+	end
+	inv:set_list("main", outlist)
 end
